b40491e7f28ce69b04ece888764ccae97dc745a4
LeNelSoN <nelis.valentin@gmail.com>
2022-01-29 11:22:00 +0100

    Disabled or Enabled button 'à faire'

--- Todolist.js
+++ Todolist.js
@@ -1,12 +1,21 @@
+var add = document.getElementById("add");
+const btn1 = document.getElementById("press")
+// if click on the button "à faire" add an element "task" in ul
 document.getElementById('press').addEventListener("click", function () {
-    let task = document.getElementById("add").value;
+    let task = add.value
     let addTask = '<li><input type="checkbox" name="check" id="' + task + '">' + task + '<button> Supprimer </button> </li>'
     document.getElementById('list')
         .insertAdjacentHTML("afterbegin", addTask)   
     });
-document.getElementsByTagName('button').addEventListener("click", function () {
-    let ul = this.parentNode;
-    ul.removeChild()
+// disable button "à faire" if input "text" is empty
+document.getElementById("add").addEventListener("keypress", () => {
+    if (add.value.length >= 1) btn1.disabled = false
+    else btn1.disabled = true;
 })
+
+// document.getElementsByTagName('button').addEventListener("click", function () {
+//     let ul = this.parentNode;
+//     ul.removeChild()
+// })
 
     
